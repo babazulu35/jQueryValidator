bb3dd13566562f824ec384bb0b5f9cdb8fecccd0
babazulu35 <aysanhakan@gmail.com>
2015-10-11 16:59:35 +0300

    Created
    files are created

---
+++ jQueryFormValidation.js
@@ -0,0 +1,27 @@
+/**
+* Jquery Simple Form Validation
+* @author Brewww Interactive
+* @package Jquery  < 1.9
+* @company Vw
+*/
+
+(function(window,$) {
+	'use strict'
+
+var VwFormValidate = function(element,options)
+{
+	this.element = element;
+	this.$element = $(element);
+	this.options = options;
+	this.metatag = this.$element.data('vwvalidate');
+}
+var defaults = {
+
+}
+VwFormValidate.prototype.initialize = function()
+{
+	this.config = $.extend({},defaults,this.options,this.metatag);
+	return this;
+}
+
+});	
